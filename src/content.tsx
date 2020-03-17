bd7aa5b76589fbf2b6865da039cd149c08116e72
zhanghe <zhanghecool@outlook.com>
2020-03-17 14:11:41 +0800

    feat(content.tsx): add background control content

--- src/content.tsx
+++ src/content.tsx
@@ -12,5 +12,20 @@ function Main() {
 
 const app = document.createElement("div");
 app.id = "my-extension-root";
+app.style.display = "none";
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.message === "clicked_browser_action") {
+    toggle();
+  }
+});
+
+function toggle() {
+  if (app.style.display === "none") {
+    app.style.display = "block";
+  } else {
+    app.style.display = "none";
+  }
+}
+
 document.body.appendChild(app);
 ReactDOM.render(<Main />, app);
